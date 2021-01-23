b3d91953148a5c894d3b50fa27039376fafc4e8f
mlesterdampios <mlesterdampios@gmail.com>
2021-01-23 15:05:29 +0800

    3:05pm

--- application/controllers/Products.php
+++ application/controllers/Products.php
@@ -15,5 +15,42 @@ class Products extends CI_Controller {
        $this->load->view('products/list',$data);
        $this->load->view('includes/footer');
    }
+
+   public function create()
+   {
+      $this->load->view('includes/header');
+      $this->load->view('products/create');
+      $this->load->view('includes/footer');      
+   }
+
+   public function store()
+   {
+       $products=new ProductsModel;
+       $products->insert_product();
+       redirect(base_url('products'));
+    }
+
+   public function edit($id)
+   {
+       $product = $this->db->get_where('products', array('id' => $id))->row();
+
+       $this->load->view('includes/header');
+       $this->load->view('products/edit',array('product'=>$product));
+       $this->load->view('includes/footer');   
+   }
+
+   public function update($id)
+   {
+       $products=new ProductsModel;
+       $products->update_product($id);
+       redirect(base_url('products'));
+   }
+
+   public function delete($id)
+   {
+       $this->db->where('id', $id);
+       $this->db->delete('products');
+       redirect(base_url('products'));
+   }
 }
 ?>
